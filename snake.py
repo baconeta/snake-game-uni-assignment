6f1ec59259f374f7d4c2f310eafcaf76f8f2aaf2
Josh Pearson <joshuapearsonnz@gmail.com>
2020-09-06 23:01:43 +1200

    added rudimentary scoring

--- snake.py
+++ snake.py
@@ -19,17 +19,19 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 # Screen size
-height = 600
-width = 600
+game_screen_height = 600
+game_screen_width = 600
+screen_height = 650
+screen_width = 600
 
 # Margin between each segment
 segment_margin = 3
 
 # Set the width and height of each snake segment
-segment_width = min(height, width) / 40 - segment_margin
-segment_height = min(height, width) / 40 - segment_margin
-total_segments_w = int(width / (segment_width + segment_margin))
-total_segments_h = int(width / (segment_width+segment_margin))
+segment_width = min(game_screen_height, game_screen_width) / 40 - segment_margin
+segment_height = min(game_screen_height, game_screen_width) / 40 - segment_margin
+total_segments_w = int(game_screen_width / (segment_width + segment_margin))
+total_segments_h = int(game_screen_width / (segment_width + segment_margin))
 
 # Set initial speed
 x_change = segment_width + segment_margin
@@ -40,7 +42,7 @@ possible_obstacles = [
     [[-1, 0], [0, 0], [1, 0], [1, 1], [2, 1], [3, 1], [3, 2], [3, 3]],
     [[0, 0], [0, 1], [0, 2], [0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [4, 2], [4, 1], [4, 0]],
     [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [1, 1], [1, 2], [1, 3], [1, 4], [2, 2], [2, 3], [2, 4], [3, 3], [3, 4], [4, 4]],
-    [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4]]
+    [[0, 0], [1, 1], [2, 2], [3, 3]]
 ]
 number_of_obstacles = random.randint(4, 7)
 # TODO add some way to make sure obstacles are placed not too closely
@@ -148,8 +150,8 @@ class Obstacle:
         # Randomly choose which obstacle is drawn
         random_obstacle = random.randint(0, len(possible_obstacles)-1)
         #  Choose a block number to have the origin spot from (the numbers are based on obstacle shapes for now)
-        origin_block_x = random.randint(1, total_segments_w - 3)
-        origin_block_y = random.randint(3, total_segments_h - 3)
+        origin_block_x = random.randint(1, total_segments_w - 4)
+        origin_block_y = random.randint(4, total_segments_h - 4)
         for x in possible_obstacles[random_obstacle]:
             rel_x = (origin_block_x + x[0]) * (segment_width + segment_margin)
             rel_y = (origin_block_y + x[1]) * (segment_height + segment_margin)
@@ -195,7 +197,7 @@ def check_snake_collisions():
 
 
 def check_snake_head_onscreen(head_x, head_y):
-    return 0 <= head_x <= width - segment_width and 0 <= head_y <= height - segment_height
+    return 0 <= head_x <= game_screen_width - segment_width and 0 <= head_y <= game_screen_height - segment_height
 
 
 def process_input():
@@ -226,8 +228,9 @@ def game_play_drawing():
     my_snake.snake_pieces.draw(screen)
     food_onscreen.food_items.draw(screen)
     obstacles.draw(screen)
+    draw_score()
     if game_lost:
-        game_over_text = font.render("Game Over", True, WHITE, BLACK)
+        game_over_text = game_over_font.render("Game Over", True, WHITE, BLACK)
         text_rect = game_over_text.get_rect()
         text_x = screen.get_width() / 2 - text_rect.width / 2
         text_y = screen.get_height() / 2 - text_rect.height / 2
@@ -235,11 +238,19 @@ def game_play_drawing():
     pygame.display.flip()
 
 
+def draw_score():
+    score_text = score_font.render("Score: " + str(current_score), True, WHITE)
+    score_text_rect = score_text.get_rect()
+    score_text_rect.center = (150, 630)
+    pygame.draw.line(screen, WHITE, (0, game_screen_height), (screen_width, game_screen_height), 1)
+    screen.blit(score_text, score_text_rect)
+
+
 # Call this function so the Pygame library can initialize itself
 pygame.init()
 
 # Create a 600x600 sized screen
-screen = pygame.display.set_mode([width, height])
+screen = pygame.display.set_mode([screen_width, screen_height])
 
 # Set the title of the window
 pygame.display.set_caption('Snake Game')
@@ -258,7 +269,9 @@ food_onscreen = Food()
 clock = pygame.time.Clock()
 game_quit = False
 game_lost = False
-font = pygame.font.Font(None, 72)
+game_over_font = pygame.font.Font(None, 72)
+score_font = pygame.font.SysFont("Courier", 48)
+current_score = 0
 
 while not game_quit:
     # Game loop
